b480d420daabc9ddb77f59c1c9572e157ec05681
Martin Batalla <martinbatalla016@gmail.com>
2026-05-16 00:39:33 -0400

    fix: prevent all jobs starting at same time to avoid crashing

--- all_main_pipeline.sh
+++ all_main_pipeline.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 # all_main_pipeline.sh
-# Submit chloroplast pipeline jobs for all samples automatically
 
 REF=$1
+if [[ -z "$REF" ]]; then echo "Usage: path/to/all_main_pipeline.sh <REF_NAME>"; exit 1; fi
 
-# Make sure logs directory exists
+# 1. Setup sample list
+ls *_R1_001.fastq.gz | sed -E 's/([-_]?R1_.*)//' > CPsamples.txt
+NUM_SAMPLES=$(wc -l < CPsamples.txt)
 mkdir -p logs
 
-# Make a text file with a list of all the samples
-ls *_R1_001.fastq.gz | sed -E 's/([-_]?R1.*)//' > CPsamples.txt
-NUM_SAMPLES=$(wc -l < CPsamples.txt)
-
-echo "Created CPsamples.txt for $NUM_SAMPLES samples"
-
-sbatch <<EOF
+# 2. Submit the Array Job and capture the Job ID
+JOB_ID=$(sbatch <<EOF | grep -oP "\d+"
 #!/bin/bash
 #SBATCH --job-name=cp_array
 #SBATCH --array=1-${NUM_SAMPLES}%20
@@ -23,14 +20,12 @@ sbatch <<EOF
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
 
-# Extract the specific sample for THIS task
 SAMPLE=\$(sed -n "\${SLURM_ARRAY_TASK_ID}p" CPsamples.txt)
-
-# Create a link so in order to have a named log file
-ln -snf "$(pwd)/logs/\${SLURM_ARRAY_JOB_ID}_\${SLURM_ARRAY_TASK_ID}.out" "logs/\${SAMPLE}.log"
-
-
-mkdir -p logs
-
-../main_cp_pipeline.sh \$SAMPLE $REF 16
+ln -snf "\$(pwd)/logs/\${SLURM_ARRAY_JOB_ID}_\${SLURM_ARRAY_TASK_ID}.out" "logs/\${SAMPLE}.log"
+#Prevent all n jobs starting at the same time
+sleep \$((RANDOM % 150))
+bash ~/bioinformatics/cp_assembly/main_cp_pipeline.sh "\$SAMPLE" "$REF"
 EOF
+)
+
+echo "Array job submitted: $JOB_ID"
